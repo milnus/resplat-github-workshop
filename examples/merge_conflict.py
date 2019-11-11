619b44f00114ea5389cad9bc33e33779394d80ef
Vivek Katial <vivekkatial@gmail.com>
2019-11-11 11:00:07 +1100

    fix

--- examples/merge_conflict.py
+++ examples/merge_conflict.py
@@ -4,7 +4,8 @@ Example script for showing how to fix merge-conflicts
 Author: Vivek Katial
 '''
 
-first_name = "Vivek"
+title = "Mr"
+firs_name = "Vivek"
 last_name = "Katial"
 
 print(first_name + " " + last_name)
